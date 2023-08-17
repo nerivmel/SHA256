db66d87c30ae2e849d1341be0b28b114985c1d3d
manuelcalle <manuel.calle@udea.edu.co>
2023-08-17 06:39:16 -0500

    Short code execute in termial

---
+++ main.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Ingrese el valor a encriptar: ', (dataToHash) => {
+  const hashResult = calculateSHA256Hash(dataToHash);
+  console.log(`Entrada: ${dataToHash}`);
+  console.log(`Encriptación SHA-256: ${hashResult}`);
+  rl.close();
+});
+
+function calculateSHA256Hash(data) {
+  const sha256Hash = crypto.createHash('sha256');
+  sha256Hash.update(data, 'utf-8');
+  return sha256Hash.digest('hex');
+}
